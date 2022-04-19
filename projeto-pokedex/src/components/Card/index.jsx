db47ea11ee5086341cedbe91ced6555555c628af
rommelsanthiago <rommel.santhiago@gmail.com>
2022-04-19 09:33:15 -0300

    Iserido o botão de add a pokedex

--- projeto-pokedex/src/components/Card/index.jsx
+++ projeto-pokedex/src/components/Card/index.jsx
@@ -1,13 +1,34 @@
-import React from "react"
+import React, { useContext } from "react"
 
 import * as Styled from "./styles"
 import { useNavigate } from "react-router-dom"
 import { goToDetails } from "../../Routes/coodinator"
 import { Button } from "../Button"
+import GlobalStateContext from '../../Global/GlobalStateContext'
 
 const Card = (props) => {
   const navigate = useNavigate()
   const { poke } = props
+  const { pokemon, setPokemon, pokedex, setPokedex } = useContext(GlobalStateContext)
+
+  const addToPokedex = () => {
+    const pokeIndex = pokemon.findIndex(
+      (item) => item.name === poke.name
+    )
+    const newPokeList = [...pokemon]
+    newPokeList.splice(pokeIndex, 1)
+    const orderedPokemon = newPokeList.sort((a, b) => {
+      return a.id - b.id
+    })
+
+    const newPokedexList = [...pokedex, poke]
+    const orderedPokedex = newPokedexList.sort((a, b) => {
+      return a.id - b.id
+    })
+
+    setPokedex(orderedPokedex)
+    setPokemon(orderedPokemon)
+  }
 
   return (
     <Styled.Container>
@@ -16,7 +37,7 @@ const Card = (props) => {
         <Styled.Img src={poke.sprites.front_default} alt="Pokemon" />
       </Styled.Content>
       <Styled.FooterCard>
-        <Button >Add</Button>
+        <Button onClick={addToPokedex} >Add</Button>
         <Button onClick={() => goToDetails(navigate, poke.name)}>Details</Button>
       </Styled.FooterCard>
     </Styled.Container>
